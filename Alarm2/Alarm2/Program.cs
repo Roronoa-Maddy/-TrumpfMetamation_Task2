bae430a738c287bf8f4f68b1b435441d818c7820
Madhavan <madhavashanmugam28@gmail.com>
2024-12-31 08:01:27 +0530

    Deleting Newly Added Alarm

--- Alarm2/Alarm2/Program.cs
+++ Alarm2/Alarm2/Program.cs
@@ -84,6 +84,8 @@ namespace Alarm2
             Keyboard.Type(VirtualKeyShort.TAB);
             Keyboard.Type(VirtualKeyShort.RETURN);
             Thread.Sleep(5000);
+            //Deleting Newly Added Alarm
+            Keyboard.Type(VirtualKeyShort.DELETE);
             //var automation = new UIA3Automation();
             //var app = FlaUI.Core.Application.Attach("Time");
             //var mainWindow = app.GetMainWindow(automation);
