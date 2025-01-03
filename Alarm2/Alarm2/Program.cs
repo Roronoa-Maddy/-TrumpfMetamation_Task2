47312bf7ff953ba33b2e2086a58bda1a63ed4fc2
Madhavan <madhavashanmugam28@gmail.com>
2025-01-03 10:50:31 +0530

    Opening Clock Application

--- Alarm2/Alarm2/Program.cs
+++ Alarm2/Alarm2/Program.cs
@@ -1,4 +1,4 @@
-using FlaUI.Core.Input;
+﻿using FlaUI.Core.Input;
 using FlaUI.Core.WindowsAPI;
 using FlaUI.UIA3;
 using System.Diagnostics;
@@ -11,6 +11,11 @@ using OpenQA.Selenium;
 using FlaUI.Core.Tools;
 using FlaUI.Core.AutomationElements;
 using AutomationElement = FlaUI.Core.AutomationElements.AutomationElement;
+using FlaUI.Core;
+using Application = FlaUI.Core.Application;
+using FlaUI.Core.Conditions;
+using System.Windows;
+using System.Threading;
 
 
 
@@ -18,81 +23,22 @@ namespace Alarm2
 {
     internal static class Program
     {
-        //[DllImport("user32.dll", SetLastError = true)]
-        //private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
-        //[DllImport("user32.dll", SetLastError = true)]
-        //static extern bool SetForegroundWindow(IntPtr hWnd);
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
+        
         [STAThread]
         static void Main()
         {
+
            
-            Process.Start(new ProcessStartInfo
-            {
-                FileName = "cmd.exe",
-                Arguments = "/c start ms-clock:",
-                WindowStyle = ProcessWindowStyle.Hidden
-            });
-            //Navigating To alarm Tab
-            System.Threading.Thread.Sleep(5000);
-            Keyboard.Type(VirtualKeyShort.DOWN);
-            Keyboard.Type(VirtualKeyShort.RETURN);
-            Thread.Sleep(1000);
-            //Creating New Alarm
-            Keyboard.Press(VirtualKeyShort.CONTROL);
-            Keyboard.Type(VirtualKeyShort.KEY_N);
-            Keyboard.Release(VirtualKeyShort.CONTROL);
-            Thread.Sleep(1000);
-            //Hours Changing
-            Keyboard.Type(VirtualKeyShort.UP);
-            Keyboard.Type(VirtualKeyShort.UP);
-            Thread.Sleep(1000);
-            //Giving Alarm Message
-            Keyboard.Type(VirtualKeyShort.TAB);
-            Keyboard.Type(VirtualKeyShort.TAB);
-            Keyboard.Type(VirtualKeyShort.TAB);
-            Keyboard.Type(VirtualKeyShort.BACK);
-            SendKeys.SendWait("Trumpf Metamation Login Time");
-            Thread.Sleep(1000);
-            Keyboard.Type(VirtualKeyShort.TAB);
-            Keyboard.Type(VirtualKeyShort.SPACE);
-            Keyboard.Type(VirtualKeyShort.TAB);
-            Keyboard.Type(VirtualKeyShort.RETURN);
-            for (int i = 0; i < 5; i++)
-            {
-                Keyboard.Type(VirtualKeyShort.RIGHT);
-                Keyboard.Type(VirtualKeyShort.RETURN);
+            var clockApp = FlaUI.Core.Application.Launch("C:\\Program Files\\WindowsApps\\Microsoft.WindowsAlarms_11.2411.2.0_x64__8wekyb3d8bbwe\\Time");
 
-            }
-            Thread.Sleep(1000);
-            //Setting Up alarm Tone
-            Keyboard.Type(VirtualKeyShort.TAB);
-            //Keyboard.Type(VirtualKeyShort.RETURN);
-            for (int j = 0; j < 4; j++)
-            {
-                Keyboard.Type(VirtualKeyShort.DOWN);
+            
+            
+          
 
-            }
-            //Disabling Snooze
-            Thread.Sleep(1000);
-            Keyboard.Type(VirtualKeyShort.TAB);
-            Thread.Sleep(1000);
-            Keyboard.Type(VirtualKeyShort.UP);
-            Keyboard.Type(VirtualKeyShort.UP);
-            Keyboard.Type(VirtualKeyShort.TAB);
-            Keyboard.Type(VirtualKeyShort.RETURN);
-            Thread.Sleep(5000);
-            //Deleting Newly Added Alarm
-            Keyboard.Type(VirtualKeyShort.DELETE);
-            //var automation = new UIA3Automation();
-            //var app = FlaUI.Core.Application.Attach("Time");
-            //var mainWindow = app.GetMainWindow(automation);
 
-            //var alarmsList = mainWindow.FindFirstDescendant(cf => cf.ByAutomationId("AlarmsListId"))?.AsListBox();
-            //var button = mainWindow.FindFirstDescendant(cf => cf.ByName("Add new alarm")).AsButton();
+
 
         }
+
     }
 }
